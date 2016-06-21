d9f2006d4a48d408e66a3eef4c40d4a552fa2fd1
Richard Remer <reanjr@gmail.com>
2016-06-20 22:10:30 -0700

    chain function

---
+++ chain.js
@@ -0,0 +1,31 @@
+/**
+ * Chain functions together.  Return value of one function passed to the other.
+ * If return value is array matching function arity, array will be applied.
+ * @param {...function} fns
+ * @returns {function}
+ */
+function chain(...fns) {
+    /**
+     * Function has argspec of first chain arg and returnspec of last chain arg.
+     */
+    return function() {
+        var fn,
+            args = Array.prototype.slice.call(arguments),
+            result;
+        
+        while (fn = fns.shift()) {
+            result = fn.apply(this, args);
+            
+            if (fn.length) {
+                args = result;
+                if (!(args instanceof Array) || args.length !== fn[0].length) {
+                    args = [args];
+                }
+            }
+        }
+        
+        return result;
+    };
+}
+
+module.exports = chain;
